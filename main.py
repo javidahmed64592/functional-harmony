3824d42beb4b1aeac03c78659368a09b87e2c3c0
Javid Ahmed <javidahmed@icloud.com>
2024-03-29 05:06:26 +0000

    Add argparse

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import argparse
+
 from src.scale import Scale
 
 if __name__ == "__main__":
-    scale = Scale.from_note(start_note="E", mode=1)
-    progression = [1, 4, 5, 1]
-    scale.print_chord_progression(progression=progression)
+    parser = argparse.ArgumentParser(description="Musical Scale Tool")
+    parser.add_argument("--start_note", type=str, help="Root note of scale", default="C")
+    parser.add_argument("--mode", type=int, help="Scale mode (1-7)", default=1)
+    parser.add_argument(
+        "--progression", type=int, nargs="*", help="Chord progression i.e. 1 4 5 1", default=[1, 4, 5, 1]
+    )
+    args = parser.parse_args()
+
+    scale = Scale.from_note(start_note=args.start_note, mode=args.mode)
+    scale.print_chord_progression(progression=args.progression)
