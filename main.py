27fc60ac0b1c106f9bbc9c2c9776b3224ceb4f4c
Javid Ahmed <javidahmed@icloud.com>
2024-03-31 00:17:37 +0000

    Update main

--- main.py
+++ main.py
@@ -2,16 +2,32 @@ import argparse
 
 from src.fh_wrapper import FHWrapper
 
+
+def main(args):
+    fh_wrapper = FHWrapper.initialise(config_filepath=args.config_file)
+
+    if args.scale:
+        fh_wrapper.create_scale(start_note=args.start_note, mode=args.mode)
+        if args.progression:
+            fh_wrapper.print_chord_progression(args.progression)
+    elif args.chord:
+        if args.identify_chord:
+            fh_wrapper.identify_chord(args.identify_chord)
+    else:
+        print("Must specify -scale or -chord!")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Musical Scale Tool")
-    parser.add_argument("--start_note", type=str, help="Root note of scale", default="C")
-    parser.add_argument("--mode", type=int, help="Scale mode (1-7)", default=1)
-    parser.add_argument(
-        "--progression", type=int, nargs="*", help="Chord progression i.e. 1 4 5 1", default=[1, 4, 5, 1]
-    )
-    args = parser.parse_args()
+    parser.add_argument("--config_file", type=str, help="Config file with music data", default="config/music_data.json")
 
-    fh_wrapper = FHWrapper.create_scale(
-        config_filepath="config/music_data.json", start_note=args.start_note, mode=args.mode
-    )
-    fh_wrapper.print_chord_progression(args.progression)
+    parser.add_argument("-scale", action="store_true", help="Use scale tools")
+    parser.add_argument("--start_note", type=str, help="Root note of scale")
+    parser.add_argument("--mode", type=int, help="Scale mode (1-7)")
+    parser.add_argument("--progression", type=int, nargs="*", help="Chord progression i.e. 1 4 5 1")
+
+    parser.add_argument("-chord", action="store_true", help="Extract a message from an image")
+    parser.add_argument("--identify_chord", type=str, nargs="*", help="Notes in chord")
+
+    args = parser.parse_args()
+    main(args)
